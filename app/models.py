3c70a3d900910336d019bab3c65c07714b63ad87
nina.sucic <nsucic0@gmail.com>
2025-01-23 22:13:57 +0100

    add superuser and account on admin

--- app/models.py
+++ app/models.py
@@ -14,5 +14,6 @@ class Account(models.Model):
     name = models.CharField(max_length=100)
     balance = models.DecimalField(max_digits=8, decimal_places=2, default=0)
     vendor = models.BooleanField()
+    
     def __str__(self):
         return f"{self.id} - {self.name}"
