fd89223651d06753c37ff5690361393432dcf360
nina.sucic <nsucic0@gmail.com>
2025-01-24 00:00:14 +0100

    Add login

--- project/urls.py
+++ project/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', home, name='homepage'),
     path('admin/', admin.site.urls),
     path('profile/', profile, name="profile" ),
+    path('accounts/', include("django.contrib.auth.urls"))
    
 
 ]
